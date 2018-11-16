21c71973f1a57393e2053f16716324303c1c4e29
Ry <ryanthecarl@gmail.com>
2018-11-16 11:33:24 -0800

    first commit

---
+++ channels-ex2/chat/urls.py
@@ -0,0 +1,14 @@
+# chat/urls.py
+# from django.conf.urls import url
+#
+# from . import views
+
+# chat/urls.py
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+]
